96359788bf24c37683ccf93b4cadbee6e8ebde45
Destianis Ocktavia <ocktaviadestianis@gmail.com>
2022-02-17 21:59:45 +0700

    tugas 6

---
+++ tugas6/app/config/config.php
@@ -0,0 +1,9 @@
+<?php
+//Base URL
+define('BASEURL', 'http://localhost/pertemuan_1/tugas6/public');
+//DB
+define('DB_HOST', 'localhost');
+define('DB_USER', 'root');
+define('DB_PASS', '');
+define('DB_NAME', 'phpmvc');
+?>
